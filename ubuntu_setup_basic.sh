b770679d3f87e432b07d7580f8c00c19fb75c1ec
Kai <tingkai.zhang@outlook.com>
2020-05-01 11:51:45 -0400

    add install fish shell

--- ubuntu_setup_basic.sh
+++ ubuntu_setup_basic.sh
@@ -2,3 +2,8 @@ sudo apt-get update &&\
 sudo apt-get install unzip  &&\
 sudo apt-get install wget &&\
 sudo apt install git
+
+# Install fish shell
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt-get update
+sudo apt-get install fish
