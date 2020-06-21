32c971b2bf0cd69ca6c56d16eb879a34b6a3152a
Kai <tingkai.zhang@outlook.com>
2020-06-21 14:09:31 -0400

    Update install_docker_compose.sh

--- install_docker_compose.sh
+++ install_docker_compose.sh
@@ -3,3 +3,5 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+docker-compose --version
