8b560e342b65aa7e7ea1e9253570c8be72180c91
Kai <tingkai.zhang@outlook.com>
2020-10-27 20:25:45 -0400

    Update pyenv_install.sh

--- pyenv_install.sh
+++ pyenv_install.sh
@@ -1,4 +1,4 @@
-apt get update
+apt-get update
 
 apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev \
 libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
